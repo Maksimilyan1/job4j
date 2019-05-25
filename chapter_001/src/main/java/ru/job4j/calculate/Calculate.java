46c738c1877ce9d719fe56eb5b7063029c552fe3
Maksim <maksrisk@gmail.ru>
2019-05-25 16:22:42 +0200

    delete

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,29 +1,28 @@
 
-   package ru.job4j.calculate;
+package ru.job4j.calculate;
 
+/**
+ *Class Calculate with method main
+ *@author - Maksim Rykhsiev
+ */
+public class Calculate {
     /**
-     *Class Calculate with method main
-     *@author - Maksim Rykhsiev
-    */
-   public class Calculate {
-     /**
      *Method main principal method in our program
      *@param args - array of strings 
-    */
-   public static void main(String[] args) {
-  
-    
-     System.out.println("Hello world.");
-   }
+     */
+    public static void main(String[] args) {
 
-      
-     /**
+
+        System.out.println("Hello world.");
+    }
+
+
+    /**
      * Method echo.
      * @param name Your name.
      * @return Echo plus your name.
      */
-    public String echo(String name) {
-        return "Echo, echo, echo : " + name;
+    public String echo(String name) { return "Echo, echo, echo : " + name;
     }
 
-  }
+}
